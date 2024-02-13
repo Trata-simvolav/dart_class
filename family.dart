38db9606a30ce2ecb3a5e92d4617d3ebddf87b1b
Trata-simvolav <114504094+Trata-simvolav@users.noreply.github.com>
2024-02-13 23:51:56 +0600

    0.2
    Add code. Kind of working, but very bad. I must finish it.

--- family.dart
+++ family.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:io';
+
 class FamilyMember {
   int many = 100;
   int food = 50;
@@ -15,7 +18,7 @@ class FamilyMember {
         afforableFood
       ]; // возращает скок осталось, и сколько съел
     } else {
-      return [0, 0, 0]; // возращает скок осталось, и сколько съел
+      return [0, 0]; // возращает скок осталось, и сколько съел
     }
   }
 
@@ -67,6 +70,7 @@ class FamilyMember {
   }
 }
 
+// ---------------------------------------------------------------------------------------------------------
 class Husband extends FamilyMember {
   String name = "Alex";
   int huppenes = 100;
@@ -77,12 +81,12 @@ class Husband extends FamilyMember {
 
     List<int> result = super.eat(30 - this.hungry);
 
-    if (result[2] == 0) {
+    if (result[0] == 0 && result[1] == 0) {
       print("В доме нет еды");
       return;
     }
 
-    this.hungry -= result[1];
+    this.hungry += result[1];
     print(
         '$name съел ${result[1]} единиц еды. Осталось голода: ${this.hungry}.');
   }
@@ -104,6 +108,7 @@ class Husband extends FamilyMember {
   }
 }
 
+// ---------------------------------------------------------------------------------------------------------
 class Wife extends FamilyMember {
   String name = "Eliza";
   int huppenes = 100;
@@ -114,12 +119,12 @@ class Wife extends FamilyMember {
 
     List<int> result = super.eat(30 - this.hungry);
 
-    if (result[2] == 0) {
+    if (result[0] == 0 && result[1] == 0) {
       print("В доме нет еды");
       return;
     }
 
-    this.hungry -= result[1];
+    this.hungry += result[1];
     print(
         '$name съела ${result[1]} единиц еды. Осталось голода: ${this.hungry}.');
   }
@@ -160,16 +165,82 @@ class Wife extends FamilyMember {
   }
 }
 
+// ---------------------------------------------------------------------------------------------------------
 class Actions {
+  String lastnameFamily;
+
   FamilyMember familyMember;
   Husband husband;
   Wife wife;
-  Actions()
+  Actions(this.lastnameFamily)
       : familyMember = FamilyMember(),
         husband = Husband(),
         wife = Wife();
 
-  void alive() {}
+  bool alive() {
+    if (husband.hungry > 0 &&
+        husband.huppenes > 0 &&
+        wife.hungry > 0 &&
+        wife.huppenes > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void followLiveForHusband({int condition = 0}) {
+    if (husband.hungry <= 10 || condition == 1) {
+      husband.eating();
+      // print("${husband.name} поел");
+    } else if (husband.huppenes <= 10 || condition == 2) {
+      husband.playing();
+      // print("${husband.name} поиграл в WoT");
+    } else if (familyMember.many <= 20 || condition == 3) {
+      husband.working();
+      // print("${husband.name} поработал");
+    } else {
+      int randomNumber = Random().nextInt(3) + 1;
+      this.followLiveForHusband(condition: randomNumber);
+    }
+  }
+
+  void followLiveForWife({int condition = 0}) {
+    if (wife.hungry <= 10 || condition == 1) {
+      wife.eating();
+      // print("${wife.name} поела");
+    } else if (wife.huppenes <= 10 || condition == 2) {
+      wife.buyShuba();
+      // print("${wife.name} купила шубу");
+    } else if (familyMember.food <= 10 || condition == 3) {
+      wife.buyFood();
+      // print("${wife.name} купила еды в дом");
+    } else if (familyMember.dirt >= 80 || condition == 4) {
+      wife.cleaning();
+      // print("${wife.name} убралась");
+    } else {
+      int randomNumber = Random().nextInt(4) + 1;
+      this.followLiveForWife(condition: randomNumber);
+    }
+  }
 }
 
-void main() {}
+void main() {
+  File file = File('output.txt');
+  IOSink sink = file.openWrite();
+
+  Actions actionPanel = Actions("Воронины");
+
+  for (int i = 1; i < 356; i++) {
+    sink.writeln("-----------------------------------------");
+    sink.writeln('День $i');
+
+    actionPanel.followLiveForHusband();
+    actionPanel.followLiveForWife();
+    if (!actionPanel.alive()) {
+      print(
+          "Кто-то в семьи ${actionPanel.lastnameFamily} умер. Игра не пройдена! Попробуй ещё раз!");
+      return;
+    }
+  }
+  sink.close();
+}
